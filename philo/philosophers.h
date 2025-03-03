2af8eeddc7cf97f1ce157b411fab747f728f728c
mathispeyre <90681104+mpeyre-s@users.noreply.github.com>
2025-03-03 11:41:38 +0100

    init base files

---
+++ philo/philosophers.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philosophers.h                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mathispeyre <mathispeyre@student.42.fr>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/03 11:36:56 by mathispeyre       #+#    #+#             */
+/*   Updated: 2025/03/03 11:38:44 by mathispeyre      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILOSOPHERS_H
+# define PHILOSOPHERS_H
+
+# include <pthread.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <sys/time.h>
+# include <unistd.h>
+# include <limits.h>
+
+#endif
