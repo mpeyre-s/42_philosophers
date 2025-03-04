f4f83f6662e02352599a82614652b36d0b1e37b3
mathispeyre <90681104+mpeyre-s@users.noreply.github.com>
2025-03-04 11:06:30 +0100

    time/clock functions

--- philo/time.c
+++ philo/time.c
@@ -6,32 +6,37 @@
 /*   By: mathispeyre <mathispeyre@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/03 11:41:04 by mathispeyre       #+#    #+#             */
-/*   Updated: 2025/03/03 15:48:22 by mathispeyre      ###   ########.fr       */
+/*   Updated: 2025/03/04 11:05:37 by mathispeyre      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philosophers.h"
 
 /**
- * @brief Extremely precise usleep function
- * @param microseconds (unsigned long)
- * @return (int) 0 for succes, -1 for error
+ * @brief Extremely precise sleep function in milliseconds
+ * @param milliseconds (unsigned long)
+ * @return (int) 0 for success, -1 for error
  */
-int	ft_usleep(unsigned long microseconds)
+int	ft_msleep(unsigned long milliseconds)
 {
 	struct timeval	start, end;
 	unsigned long	elapsed;
 
-	if (!microseconds)
+	if (!milliseconds)
 		return (-1);
 	gettimeofday(&start, NULL);
-	usleep(microseconds);
+	usleep(milliseconds * 1000);
 	gettimeofday(&end, NULL);
-	elapsed = (end.tv_sec - start.tv_sec) * 1000000 + (end.tv_usec - start.tv_usec);
-	if (elapsed < microseconds)
-		usleep(microseconds - elapsed);
-	// gettimeofday(&end, NULL);
-	// elapsed = (end.tv_sec - start.tv_sec) * 1000000 + (end.tv_usec - start.tv_usec);
-	// printf("ft_usleep : %lu µs (requested: %lu)\n", elapsed, microseconds);
+	elapsed = (end.tv_sec - start.tv_sec) * 1000 + (end.tv_usec - start.tv_usec) / 1000;
+	if (elapsed < milliseconds)
+		usleep((milliseconds - elapsed) * 1000);
 	return (0);
+}
+
+time_t	get_time_in_ms(void)
+{
+	struct timeval		time;
+
+	gettimeofday(&time, NULL);
+	return ((time.tv_sec * 1000) + (time.tv_usec / 1000));
 }
